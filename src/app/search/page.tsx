d73156c69bcdc35067c687740d39591b859395db
Strikan <buljevic.juraj@gmail.com>
2024-01-23 18:41:35 +0100

    adding actions for mapbox search

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [searchParams, setSearchParams] = useState();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+}
