491b3c9a8322221a116c6c3d63ad1e2960ad112b
Strikan <buljevic.juraj@gmail.com>
2024-02-03 15:01:07 +0100

    removing unused imports

--- src/components/search/MainSearchModal.tsx
+++ src/components/search/MainSearchModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Search, X } from 'lucide-react';
